c8f2ad3471dbf8a3bd5b9f57a79d25b0b9b0f32e
Raphael Thibierge <raph.t.id@gmail.com>
2017-10-21 12:57:58 -0400

    Add sentry-mongodb

--- config/sentry.php
+++ config/sentry.php
@@ -11,4 +11,28 @@ return array(
 
     // Capture default user context
     'user_context' => true,
+
+
+    /*
+     * Mongodb-sentry
+     */
+
+
+    'groups' => array(
+
+        'model' => 'Jenssegers\Mongodb\Sentry\Group',
+
+    ),
+
+    'users' => array(
+
+        'model' => 'Jenssegers\Mongodb\Sentry\User',
+
+    ),
+
+    'throttling' => array(
+
+        'model' => 'Jenssegers\Mongodb\Sentry\Throttle',
+
+    ),
 );
