771d36269655959074cf10b388aa2119b946fe31
Raphael Thibierge <raph.t.id@gmail.com>
2017-10-21 20:01:50 -0400

    Dialogflow integration

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\BotMan\Middleware\ApiAi;
 use BotMan\Drivers\Facebook\FacebookDriver;
 use Illuminate\Http\Request;
 
@@ -27,10 +28,29 @@ class BotManController extends Controller
         // Create BotMan instance
         $botman = BotManFactory::create($config);
 
-        // Default message
-        $botman->hears('Hi', function (BotMan $bot){
-            $bot->types();
-            $bot->reply('Hello !');
+        // Dialogflow API
+        $dialogflow = ApiAi::create(env('DIALOGFLOW_API_KEY'))->listenForAction();
+
+        // Apply global "received" middleware
+        $botman->middleware->received($dialogflow);
+
+        $botman->hears('input.welcome', function (BotMan $bot) {
+            // The incoming message matched the "input.welcome" on Dialoglfow.com
+            // Retrieve API.ai information:
+            $extras = $bot->getMessage()->getExtras();
+
+            // response content
+            $apiReply = $extras['apiReply'];
+            $apiAction = $extras['apiAction'];
+            $apiIntent = $extras['apiIntent'];
+            $apiParameters = $extras['apiParameters'];
+
+            $bot->reply($apiReply);
+        })->middleware($dialogflow);
+
+        // default response
+        $botman->fallback(function (BotMan $bot){
+            $bot->reply("I didn't get that..");
         });
 
         // start listening
